9b6d5aac333bf66498d20bd2aeec33b4a3647f62
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add CalyxOS & incremental versions to tombstones
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    [mkbestas]: Rewrite for T
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08

--- debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
+++ debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -632,6 +632,8 @@ static void print_guest_thread(CallbackType callback, SymbolizeCallbackType symb
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback,
                              SymbolizeCallbackType symbolize) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
+  CBL("CalyxOS version: '%s'", tombstone.calyxos_version().c_str());
+  CBL("Build incremental version: '%s'", tombstone.build_version_incremental().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   if (!tombstone.kernel_release().empty()) {
     CBL("Kernel Release: '%s'", tombstone.kernel_release().c_str());
